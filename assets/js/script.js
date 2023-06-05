aa068c673fbf3fb13105b3df54fa125311009d62
stellaric <90323575+stellaric@users.noreply.github.com>
2023-06-05 08:00:46 +0000

    modification 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,30 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+
+
+
+//--------------Carousel--------------
+
+// Exécute le code une fois que la page HTML est chargée
+$(document).ready(function() {
+  // Sélectionne tous les éléments HTML qui ont la classe "owl-carousel" et initialise le plugin Owl Carousel
+  $(".owl-carousel").owlCarousel({
+    // Le carrousel ne montrera qu'un élément à la fois
+    items: 1,
+    // Le carrousel bouclera lorsqu'il arrive au dernier élément
+    loop: true,
+    // Le carrousel défilera automatiquement.
+    autoplay: true,
+    // Définit la vitesse à laquelle les images défilent
+    autoplaySpeed: 1000,
+    // Définit la vitesse de transition entre les images
+    smartSpeed: 500,
+    // Arrête le défilement lorsque l'utilisateur passe sa souris sur un élément
+    autoplayHoverPause: true,
+  });
+});
+
 //-----------------DROPDOWN-------------------------------------
 
 /*Quand on clique sur le boutton le toggle cahcé apparait*/
@@ -70,4 +94,13 @@ for (var i = 0; i < btns.length; i++) {
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
+}
+
+
+
+// modal crée par stella 
+
+var img_modal
+function Modal(img_modal){
+document.getElementById(img_modal).style.display='block'
 }
