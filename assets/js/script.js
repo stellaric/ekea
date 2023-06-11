d65f83d301cbf047362b5563f27a2677fcf749fe
Jade-dev-28 <jade.jsa@gmail.com>
2023-06-11 23:03:45 +0200

    maj page produits

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,6 +107,64 @@ for (var i = 0; i < btns.length; i++) {
 
 /*___________________REGEX_______*/
 
+
+// Fonction de validation du formulaire
+function validateForm() {
+  // Récupérer les valeurs des champs
+  var firstName = document.getElementById("fname").value;
+  var lastName = document.getElementsByName("lname")[0].value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementsByName("fname")[1].value;
+  var message = document.getElementById("message").value;
+
+  // Regex pour les critères de validation
+  var nameRegex = /^[A-Z -]+$/;
+  var emailRegex = /^[a-z0-9@]+$/i;
+  var subjectRegex = /^[A-Z0-9 +-]+$/i;
+
+  // Vérifier les conditions de validation
+  if (firstName === "" || !nameRegex.test(firstName)) {
+    document.getElementById("error").innerHTML = "Le prénom est invalide";
+    return false;
+  }
+
+  if (!nameRegex.test(lastName)) {
+    document.getElementById("error").innerHTML = "Le nom est invalide";
+    return false;
+  }
+
+  if (email === "" || !emailRegex.test(email)) {
+    document.getElementById("error").innerHTML = "L'email est invalide";
+    return false;
+  }
+
+  if (!subjectRegex.test(subject)) {
+    document.getElementById("error").innerHTML = "Le sujet est invalide";
+    return false;
+  }
+
+  if (message === "" || !subjectRegex.test(message)) {
+    document.getElementById("error").innerHTML = "Le message est invalide";
+    return false;
+  }
+
+  // Si toutes les conditions sont respectées, le formulaire peut être soumis
+  return true;
+}
+
+// Écouter l'événement de soumission du formulaire
+document.getElementById("myForm").addEventListener("submit", function () {
+  // Valider le formulaire
+  if (!validateForm()) {
+    // Empêcher le comportement par défaut du formulaire
+    return false;
+  }
+});
+
+
+
+
+
 /*let myForm = document.getElementById('myForm');
 
 
@@ -132,3 +190,32 @@ else if(myRegex.test(myInput.value) === false ) {
 
 
 /*________page prduits______*/
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
