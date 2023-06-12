5976256c13ba5fe54948ca94bd327d9692a826f1
stellaric <90323575+stellaric@users.noreply.github.com>
2023-06-12 09:01:20 +0000

    modification 9

--- assets/js/script.js
+++ assets/js/script.js
@@ -151,3 +151,22 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
 }
 
+
+/**_______________tabs_____________ */
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
