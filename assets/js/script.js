14505e04c54ccb5b916ccbd1d0e0cf91bfa832e1
stellaric <stella.richal.sio@gmail.com>
2023-06-14 05:49:26 +0200

    version finale 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,9 @@ function openNav() {
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0%";
+    console.log("Fermeture du menu"); // Vérifiez si ce message est affiché dans la console du navigateur
+    document.getElementById("mySidenav").style.width = "0";
 }
-
-
 
 //_______________________CARROUSEL___________________________________
 //page accueil & service (carrousel)
@@ -46,6 +45,9 @@ next();
 
 // Establecemos un intervalo para mostrar la siguiente diapositiva cada 3 segundos
 setInterval(next, 3000);
+
+
+
 
 
 
@@ -128,117 +130,59 @@ for (var i = 0; i < btns.length; i++) {
 /*___________________REGEX_______*/
 
 
-// Fonction de validation du formulaire
-function validateForm() {
-    // Récupérer les valeurs des champs
-    var firstName = document.getElementById("fname").value;
-    var lastName = document.getElementsByName("lname")[0].value;
-    var email = document.getElementById("email").value;
-    var subject = document.getElementsByName("fname")[1].value;
-    var message = document.getElementById("message").value;
 
-    // Regex pour les critères de validation
-    var nameRegex = /^[A-Z -]+$/;
-    var emailRegex = /^[a-z0-9@]+$/i;
-    var subjectRegex = /^[A-Z0-9 +-]+$/i;
+function validateForm(event) {
+    event.preventDefault(); // Empêche la soumission par défaut du formulaire
 
-    // Vérifier les conditions de validation
-    if (firstName === "" || !nameRegex.test(firstName)) {
-        document.getElementById("error").innerHTML = "Le prénom est invalide";
-        return false;
+    // Récupérer les données du formulaire
+    var nom = document.getElementById('nom').value;
+    var prenom = document.getElementById('prenom').value;
+    var email = document.getElementById('email').value;
+    var sujet = document.getElementById('sujet').value;
+    var message = document.getElementById('message').value;
+
+    // Expressions régulières pour la validation
+    var nameRegex = /^[a-zA-Z\s]+$/;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Validation du nom
+    if (!nameRegex.test(nom)) {
+        showError('name', 'Le nom n\'est pas valide. Veuillez utiliser uniquement des lettres.');
+        return;
     }
 
-    if (!nameRegex.test(lastName)) {
-        document.getElementById("error").innerHTML = "Le nom est invalide";
-        return false;
+    // Validation de l'email
+    if (!emailRegex.test(email)) {
+        showError('email', 'L\'email n\'est pas valide. Veuillez entrer une adresse email correcte.');
+        return;
     }
 
-    if (email === "" || !emailRegex.test(email)) {
-        document.getElementById("error").innerHTML = "L'email est invalide";
-        return false;
+    // Validation du sujet
+    if (subject.trim() === '') {
+        showError('subject', 'Veuillez entrer un sujet.');
+        return;
     }
 
-    if (!subjectRegex.test(subject)) {
-        document.getElementById("error").innerHTML = "Le sujet est invalide";
-        return false;
+    // Validation du message
+    if (message.length < 10) {
+        showError('message', 'Le message doit contenir au moins 10 caractères.');
+        return;
     }
 
-    if (message === "" || !subjectRegex.test(message)) {
-        document.getElementById("error").innerHTML = "Le message est invalide";
-        return false;
-    }
-
-    // Si toutes les conditions sont respectées, le formulaire peut être soumis
-    return true;
+    // Toutes les validations sont passées, soumission du formulaire
+    // Remplacez l'action par l'URL de l'endroit où vous souhaitez envoyer les données du formulaire
+    document.getElementById('contactForm').action = "mailto:stella.richal.pro@gmail.com";
+    document.getElementById('contactForm').submit();
 }
 
-// Écouter l'événement de soumission du formulaire
-document.getElementById("myForm").addEventListener("submit", function() {
-    // Valider le formulaire
-    if (!validateForm()) {
-        // Empêcher le comportement par défaut du formulaire
-        return false;
-    }
-});
-
-
-
-
-
-/*let myForm = document.getElementById('myForm');
-
-
-myForm.addEventListener('submit', function(e) {
-let myInput = document.getElementById('fname');
-let myRegex = /^[a-zA-Z-\s]+$/;
-
-if (myInput.value.trim() === "") {
-   let myError = document.getElementById('error');
-    myError.innerHTML = "Le champs prénom est requis.";
-   myError.style.color ='red';
-    e.preventDefault();
+function showError(field, message) {
+    var errorElement = document.createElement('p');
+    errorElement.className = 'error';
+    errorElement.innerText = message;
+    var inputElement = document.getElementById(field);
+    inputElement.parentNode.insertBefore(errorElement, inputElement.nextSibling);
 }
 
-else if(myRegex.test(myInput.value) === false ) {
-  let myError = document.getElementById('error');
-  myError.innerHTML = " Le prénom doit comporter des lettres des tirest uniquements.";
- myError.style.color ='red';
- e.preventDefault();
-}
-
-});*/
-
-
-/*________page prduits______*/
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-}
-
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("myProd");
-    if (n > slides.length) { slideIndex = 1; }
-    if (n < 1) { slideIndex = slides.length; }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-}
 
 /**_______________tabs_____________ */
 function openTab(event, tabId) {
